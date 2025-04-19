abcca23d7c899a17b0ce155bdb9572e19c1b7724
LucasMagalhaes14 <lucasbonomi@icloud.com>
2025-04-18 21:46:04 -0300

    Add files via upload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components/native';
 import { StatusBar } from 'react-native';
+import styled from 'styled-components/native';
 import theme from '../styles/theme';
 
 export const HeaderContainer = styled.View`
